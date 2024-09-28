aa4d8e53215d3000dd8fe64200fcd7140d08ca49
Saurabh Saran <saurabhsaran@pythonerd.com>
2024-09-28 00:39:29 -0600

    foo

--- DataStructures/Functions/lambda_example.py
+++ DataStructures/Functions/lambda_example.py
@@ -26,3 +26,18 @@ Output:
 from math import sqrt
 square_root = lambda x: sqrt(x)
 print(square_root(100))
+
+
+## lambda functions can be passed as paramters to a regular function
+## but lambdas are single expression functions, so you cannot do assignments like x = 5 for example
+
+
+def calculate_square_root(x, fn):
+    return fn(x)
+
+def calculate_square(y, fn):
+    return fn(y)
+
+g = calculate_square_root(3, square_root)
+h = calculate_square(3, square)
+print("square root: {} and square: {}".format(g, h)) 
