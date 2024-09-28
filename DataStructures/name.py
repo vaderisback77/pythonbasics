357bc2259bc2ad258f78f32e45efb3f444586e0e
Saurabh Saran <saurabhsaran@pythonerd.com>
2024-09-27 22:41:25 -0600

    first program

---
+++ DataStructures/name.py
@@ -0,0 +1,9 @@
+name = input("Enter name:\n")
+if name:
+    print(f"Thanks for entering your name {name}. Goodbye")
+else:
+    print("No name entered")
+print("Program ended.")
+
+## if there is an empty string, the expression evaluates to zero
+
