564e4c0a64632410ea89dd2feca5a2f1011721f1
Saurabh Saran <saurabhsaran@pythonerd.com>
2024-09-30 20:09:16 -0600

    new reduce function

--- DataStructures/Functions/reduce_example.py
+++ DataStructures/Functions/reduce_example.py
@@ -12,3 +12,14 @@ Output
 29
 python is awesome
 '''
+
+## this program checks for the any or all truthy condition
+## In ANY condition, if any element in the iterable is truthy, the result is True
+## In ALL condition, ONLY if all the elements in the iterable are truthy, the result will be false
+l = [0, "", None, 100]
+result = reduce(lambda x, y: bool(x) or bool(y), l)
+result_2 = reduce(lambda x, y: bool(x) and bool(y), l)
+print(result, result_2)
+
+
+
