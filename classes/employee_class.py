ac9775018ad4e3775af08b124fc0a3ac621f6d56
Saurabh Saran <saurabhsaran@Saurabhs-MacBook-Air.local>
2024-11-29 12:29:05 -0700

    classes

---
+++ classes/employee_class.py
@@ -0,0 +1,27 @@
+#! usr/bin/python3
+
+class Employee:
+	location = "United States"
+	raise_amount = 0.05
+
+	def __init__(self, first, last, employee_id, pay, location):
+		self.first = first
+		self.last = last
+		self.employee_id = employee_id
+		self.location = Employee.location
+		self.pay = pay
+		self.location = location
+
+	def fullname(self):
+		return self.first + " " + self.last
+
+	def apply_raise(self):
+		self.pay += self.pay * self.raise_amount
+		return self.pay 
+
+
+employee_1 = Employee(first="Saurabh", last="Saran", employee_id=138184, pay=100000, location="United States")
+print(employee_1.fullname())
+print("Salary before raise {}".format(employee_1.pay))
+employee_1.apply_raise()
+print("Salary after raise {}".format(employee_1.pay))
